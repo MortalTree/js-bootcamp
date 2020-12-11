fa0d27835b56d591310cd3e9f30e03654ec83dfa
MortalTree <49521183+MortalTree@users.noreply.github.com>
2020-12-11 21:34:50 +0200

    Add files via upload

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -11,16 +11,45 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-// add an event listener
-document.querySelector('button').addEventListener('click', function (e) {
-    // console.log('Did this work?')
-    // console.log(e)
-    // e.target.textContent = 'The button was clicked'
+const filters = {
+    searchText: ''
+}
+
+// takes all notes and functions, figures out which note matches the search text
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    // clear the div
+    document.querySelector('#notes').innerHTML = ''
+
+    // add the new filtered div
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+// call it here to make sure all notes show initially
+renderNotes(notes, filters)
+
+// add click listener
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'    
 })
 
+document.querySelector('#remove-all').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
+})
 
-
-
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
 
 
 
